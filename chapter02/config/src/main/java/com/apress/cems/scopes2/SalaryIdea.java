57d160dcd57510fbd4be094fa472a3e2bff4f4df
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-11-21 11:56:35 +0100

    Add demo to experiment with Spring beans @Scope(prototype)

--- chapter02/config/src/main/java/com/apress/cems/scopes2/SalaryIdea.java
+++ chapter02/config/src/main/java/com/apress/cems/scopes2/SalaryIdea.java
@@ -11,8 +11,8 @@ public interface SalaryIdea {
     Integer getAmount();
 
     //de-comment this and the lines in the test to witness the proxying of a default method
-    /*default Integer defaultMethod() {
+    default Integer defaultMethod() {
         System.out.println("Called from here ->" + this.hashCode() );
         return 0;
-    }*/
+    }
 }
